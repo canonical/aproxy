a65995eda1f80c705e9cd53e945bc21f1376585b
Weii Wang <weii.wang@canonical.com>
2023-10-17 16:40:33 +0800

    Refactor code and improve HTTP relay

--- aproxy_test.go
+++ aproxy_test.go
@@ -1,98 +1,60 @@
 package main
 
 import (
-	"bufio"
 	"encoding/hex"
-	"errors"
+	"io"
 	"net"
-	"net/http"
-	"sync"
 	"testing"
 )
 
-func TestTlsConn(t *testing.T) {
-	ccRemote, ccLocal := net.Pipe()
-	tc := TlsConn{
-		Conn: ccLocal,
-		DstAddr: &net.TCPAddr{
-			IP:   make(net.IP, 4),
-			Port: 443,
-		},
-	}
-	// data obtained from https://gitlab.com/wireshark/wireshark/-/blob/master/test/captures/tls12-aes256gcm.pcap
-	clientHello, _ := hex.DecodeString("160301004f0100004b0303588e60d1d96bad5f1fcf0b8818466257d73385bdaaed0ac4bfd7228a6da059ad00000200a9010000200005000501000000000000000e000c0000096c6f63616c686f7374ff01000100")
-	go ccRemote.Write(clientHello)
-	host, err := tc.Host()
-	if host != "localhost:443" || err != nil {
-		t.Fatalf("failed to preread SNI from TLS client hello")
-	}
-	proxyRemote, proxyLocal := net.Pipe()
-	var proxyErr error
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		connectRequest, err := http.ReadRequest(bufio.NewReaderSize(proxyRemote, 0))
-		if err != nil {
-			proxyErr = err
-			return
-		}
-		if connectRequest.Method != "CONNECT" {
-			proxyErr = errors.New("proxy didn't receive HTTP CONNECT request")
-			return
-		}
-		_, _ = proxyRemote.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
-		clientHelloRecv := make([]byte, len(clientHello))
-		n, err := proxyRemote.Read(clientHelloRecv)
-		if n != len(clientHelloRecv) || err != nil {
-			proxyErr = errors.New("proxy didn't receive replayed TLS client hello")
-		}
-	}()
-	err = tc.PrepareTunnel(proxyLocal)
-	wg.Wait()
-	if proxyErr != nil {
-		t.Fatalf(proxyErr.Error())
-	}
+func TestPrereadConn(t *testing.T) {
+	remote, local := net.Pipe()
+	go remote.Write([]byte("hello, world"))
+	preread := &PrereadConn{conn: local}
+	preread.StartPreread()
+	buf := make([]byte, 5)
+	_, err := preread.Read(buf)
 	if err != nil {
-		t.Fatalf("failed to prepare proxy tunnel connection: %s", err)
+		t.Fatalf("Read failed during preread: %s", err)
+	}
+	buf = make([]byte, 3)
+	_, err = preread.Read(buf)
+	if err != nil {
+		t.Fatalf("Read failed during preread: %s", err)
+	}
+	preread.RestorePreread()
+	buf2 := make([]byte, 12)
+	_, err = io.ReadFull(preread, buf2)
+	if err != nil {
+		t.Fatalf("Read failed after preread: %s", err)
+	}
+	if string(buf2) != "hello, world" {
+		t.Fatalf("preread altered the read state: got %s", string(buf2))
 	}
 }
 
-func TestHttpConn(t *testing.T) {
-	ccRemote, ccLocal := net.Pipe()
-	hc := HttpConn{
-		Conn: ccLocal,
-		DstAddr: &net.TCPAddr{
-			IP:   make(net.IP, 4),
-			Port: 80,
-		},
+func TestPrereadSNI(t *testing.T) {
+	remote, local := net.Pipe()
+	// data obtained from https://gitlab.com/wireshark/wireshark/-/blob/master/test/captures/tls12-aes256gcm.pcap
+	clientHello, _ := hex.DecodeString("160301004f0100004b0303588e60d1d96bad5f1fcf0b8818466257d73385bdaaed0ac4bfd7228a6da059ad00000200a9010000200005000501000000000000000e000c0000096c6f63616c686f7374ff01000100")
+	go remote.Write(clientHello)
+	sni, err := PrereadSNI(NewPrereadConn(local))
+	if err != nil {
+		t.Fatalf("PrereadSNI failed: %s", err)
 	}
-	go ccRemote.Write([]byte("GET / HTTP/1.1\r\nHost: example.com\r\nAccept: */*\r\n\r\n"))
-	host, err := hc.Host()
-	if host != "example.com:80" || err != nil {
-		t.Fatalf("failed to preread Host header from HTTP request")
+	if sni != "localhost" {
+		t.Fatalf("PrereadSNI returns incorrect SNI: expected: localhost, got %s", sni)
 	}
-	proxyRemote, proxyLocal := net.Pipe()
-	var proxyErr error
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		req, err := http.ReadRequest(bufio.NewReaderSize(proxyRemote, 0))
-		if err != nil {
-			proxyErr = err
-			return
-		}
-		if req.Method != "GET" || req.URL.String() != "http://example.com/" {
-			proxyErr = errors.New("proxy didn't receive correct HTTP request")
-			return
-		}
-	}()
-	if err := hc.PrepareTunnel(proxyLocal); err != nil {
-		t.Fatalf("failed to prepare proxy tunnel connection: %s", err)
+}
+
+func TestPrereadHttpHost(t *testing.T) {
+	remote, local := net.Pipe()
+	go remote.Write([]byte("GET / HTTP/1.1\r\nHost: example.com\r\nAccept: */*\r\n\r\n"))
+	host, err := PrereadHttpHost(NewPrereadConn(local))
+	if err != nil {
+		t.Fatalf("PrereadHttpHost failed: %s", err)
 	}
-	wg.Wait()
-	if proxyErr != nil {
-		t.Fatalf(proxyErr.Error())
+	if host != "example.com" {
+		t.Fatalf("PrereadHttpHost returns incorrect host: expected: example.com, got %s", host)
 	}
 }
