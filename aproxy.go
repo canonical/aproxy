cbfe75a4c9b3f0ad501719693afc62f7519fae5e
Weii Wang <weii.wang@canonical.com>
2025-06-03 21:34:40 +0800

    Handle HTTP/1.0

--- aproxy.go
+++ aproxy.go
@@ -383,7 +383,7 @@ func HandleConn(conn net.Conn, proxy string) {
 			return
 		}
 		logger.Info("relay HTTP connection to proxy")
-		RelayTCP(consigned, proxyConn, logger)
+		RelayHTTP(consigned, proxyConn, logger)
 	default:
 		logger.Error(fmt.Sprintf("unknown destination port: %d", dst.Port))
 		return
